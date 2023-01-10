6258d8c6d2cc7c9dcdfb4b42d03fd96c61ba6658
gurungpratik <pratikgurung365@gmail.com>
2023-01-10 16:06:10 +0000

    completed view for all articles

---
+++ src/components/ArticleList/ArticleList.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import "./ArticleList.css";
+import { getAllArticles } from "../../apiFunctions";
+import NavBar from "../NavBar/NavBar";
+import ArticleCard from "../ArticleCard/ArticleCard";
+
+export default function ArticleList() {
+  const [ArticleList, setArticleList] = useState([]);
+  const [isLoading, setIsLoading] = useState([true]);
+
+  useEffect(() => {
+    getAllArticles().then((data) => {
+        console.log(data, "<< this is the data")
+      setArticleList(data.articles);
+      setIsLoading(false);
+    });
+  }, []);
+  console.log(ArticleList, "this is the ArticleList")
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <main className="Articles">
+      <NavBar />
+      <ul className="Article-list">
+        {ArticleList.map((article) => {
+            return <ArticleCard key={article.article_id} {...article} />;
+        })}
+      </ul>
+    </main>
+  );
+}
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import "./NavBar.css";
+
+export default function NavBar() {
+    return (
+        <div className="NavBar">
+            <h1>All Articles</h1>
+            <h1>Coding</h1>
+            <h1>Football</h1>
+            <h1>Cooking</h1>
+        </div>
+      );
+}
