c3345c4908e1a8bccd11f81e74d6ba9bc1d05034
gurungpratik <pratikgurung365@gmail.com>
2023-01-13 13:37:14 +0000

    updated NavBar, added topics state to get & set existing topics with links to topic path

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,33 @@
 import "./NavBar.css";
+import { useState, useEffect } from "react";
+import { getTopics } from "../../apiFunctions";
+import { Link } from "react-router-dom";
 
 export default function NavBar() {
-    return (
-        <div className="NavBar">
-            <h1>All Articles</h1>
-            <h1>Coding</h1>
-            <h1>Football</h1>
-            <h1>Cooking</h1>
-        </div>
-      );
+  const [topics, setTopics] = useState([]);
+
+  useEffect(() => {
+    getTopics().then((data) => {
+      setTopics(data.topics);
+    });
+  }, []);
+
+  return (
+    <div>
+      <ul className="Topics-List">
+        <Link to={`/articles`}>
+          <h1>All articles</h1>
+        </Link>
+        {topics.map((topic) => {
+          return (
+            <li className="Topic-Item" key={topic.slug}>
+              <Link to={`/articles/${topic.slug}`}>
+                <h1>{topic.slug}</h1>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
