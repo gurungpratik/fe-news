6721a00c6a04bbb580cb5688551b2108f4be0fca
gurungpratik <pratikgurung365@gmail.com>
2023-01-13 10:39:47 +0000

    added isPosting state to provide feedback when posting a comment

--- src/components/CommentAdder/CommentAdder.jsx
+++ src/components/CommentAdder/CommentAdder.jsx
@@ -5,6 +5,7 @@ import { postComment } from "../../apiFunctions";
 export default function CommentAdder({ article_id, setCommentList }) {
   const [newComment, setNewComment] = useState("");
   const [error, setError] = useState(null);
+  const [isPosting, setIsPosting] = useState(false);
 
   const handleChange = (event) => {
     setNewComment(event.target.value);
@@ -12,15 +13,19 @@ export default function CommentAdder({ article_id, setCommentList }) {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    setIsPosting(true);
     postComment(article_id, newComment).then((data) => {
-      setCommentList((currComments) => [...currComments, data.comment]);
+        setCommentList((currComments) => [...currComments, data.comment]);
+        setIsPosting(false);
   }).catch((err) => {
-    setError("error occured")
+    setError("error occured, please try again")
+    setIsPosted(false)
   });
   setNewComment("");
 }
 
 if (error) return error;
+if(isPosting) return <p>Post in progress...</p>
 
   return (
     <form onSubmit={handleSubmit} className="Comment-Form">
