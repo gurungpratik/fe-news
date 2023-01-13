57fe727a34380bb69c7e6c6d494d9af94d3abc02
gurungpratik <pratikgurung365@gmail.com>
2023-01-13 13:26:56 +0000

    created article topic routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Header from "./components/Header/Header";
+import NavBar from "./components/NavBar/NavBar";
 import ArticleList from "./components/ArticleList/ArticleList";
 import SingleArticle from "./components/SingleArticle/SingleArticle";
 
@@ -8,10 +9,15 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <NavBar />
       <Routes>
-        <Route exact path="/" element={<Navigate to="/home"></Navigate>}></Route>
-        <Route path="/home" element={<ArticleList></ArticleList>}></Route>
+        <Route exact path="/" element={<Navigate to="/articles"></Navigate>}></Route>
+        <Route path="/articles" element={<ArticleList></ArticleList>}></Route>
         <Route path={`/articles/:article_id`} element={<SingleArticle />}></Route>
+        <Route path={`/articles`} element={<ArticleList/>}></Route>
+        <Route path={`/articles/coding`} element={<ArticleList topic="coding"/>}></Route>
+        <Route path={`/articles/football`} element={<ArticleList topic="football"/>}></Route>
+        <Route path={`/articles/cooking`} element={<ArticleList topic="cooking"/>}></Route>
       </Routes>
     </div>
   );
